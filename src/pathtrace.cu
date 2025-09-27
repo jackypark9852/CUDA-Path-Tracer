32c5bfb98cd889127e03663a0f6bc852d8c1127d
Jacky Park <jackypark9852@gmail.com>
2025-09-27 18:58:15 -0400

    feat: add stochastic anti aliasing

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -133,7 +133,16 @@ __global__ void generateRayFromCamera(Camera cam, int iter, int traceDepth, Path
         segment.ray.origin = cam.position;
         segment.color = glm::vec3(1.0f, 1.0f, 1.0f);
 
-        // TODO: implement antialiasing by jittering the ray
+        // simple antialiasing by jittering the ray
+        thrust::default_random_engine rng = MakeSeededRandomEngine(iter, index, traceDepth); 
+        thrust::uniform_real_distribution u01(-0.5f, 0.5f); 
+
+        float jitterX = u01(rng); 
+        float jitterY = u01(rng); 
+
+        float jitteredX = (float)x + jitterX; 
+        float jitteredY = (float)y + jitterY;
+
         segment.ray.direction = glm::normalize(cam.view
             - cam.right * cam.pixelLength.x * ((float)x - (float)cam.resolution.x * 0.5f)
             - cam.up * cam.pixelLength.y * ((float)y - (float)cam.resolution.y * 0.5f)
